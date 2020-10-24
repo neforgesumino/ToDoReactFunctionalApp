13b7cf647625a7389707b7c6ecae05864be9616d
neforgesumino <57845270+neforgesumino@users.noreply.github.com>
2020-10-24 20:44:20 +0300

    Updates

---
+++ precache-manifest.6e866a78fecf167c8efe53fbb782e32b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b50c52a7cbd148d056fd4d07c440efa",
+    "url": "/neforgesumino/ToDoReactFunctionalApp/index.html"
+  },
+  {
+    "revision": "4e2bf8042f647bb463cd",
+    "url": "/neforgesumino/ToDoReactFunctionalApp/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7c1ae89852b64cfe7e43",
+    "url": "/neforgesumino/ToDoReactFunctionalApp/static/css/main.d19a1b4b.chunk.css"
+  },
+  {
+    "revision": "4e2bf8042f647bb463cd",
+    "url": "/neforgesumino/ToDoReactFunctionalApp/static/js/2.77b7d905.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/neforgesumino/ToDoReactFunctionalApp/static/js/2.77b7d905.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1ae89852b64cfe7e43",
+    "url": "/neforgesumino/ToDoReactFunctionalApp/static/js/main.01bfc556.chunk.js"
+  },
+  {
+    "revision": "f9793667da2359b92e82",
+    "url": "/neforgesumino/ToDoReactFunctionalApp/static/js/runtime-main.8fe2b94c.js"
+  }
+]);
